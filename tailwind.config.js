6e815541a0eb985d8ea08a9df07170499755fe19
Alexander Cai <alexandercai@outlook.com>
2021-05-12 18:58:02 -0600

    fix weird purgecss error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  purge: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-  ],
+  purge: {
+    content: [
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './src/components/**/*.{js,ts,jsx,tsx}',
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
